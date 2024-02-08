ca46a7503f795f13fd04baf43448d39fd6a57c8d
ajkruk <alicia.j.kruk@gmail.com>
2024-02-07 22:14:07 -0500

    commit 1

---
+++ src/CakeLayer.tsx
@@ -0,0 +1,15 @@
+import './CakeLayer.css'
+interface Props {
+    layer: Layer;
+};
+
+function CakeLayer(props: Props) {
+  
+    return (
+        <div>
+            <div className="Layer" style={{height: props.layer.height, width: props.layer.width, backgroundColor: props.layer.color}}></div>
+        </div>
+    );
+};
+
+export default CakeLayer
